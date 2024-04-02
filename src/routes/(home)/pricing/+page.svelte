325a618d061252062359a01d35d46e318de0b0c2
Boe Reh <justboereh@gmail.com>
2024-04-02 09:12:07 -0500

    updated deps; prerender pages; simple pricing page

---
+++ src/routes/(home)/pricing/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Card, CardHeader, CardContent } from '$lib/components/ui/card';
+</script>
+
+<div class="p-4">
+	<div class="mx-auto max-w-7xl space-y-4">
+		<h1 class="text-center">Simple, transparent pricing</h1>
+		<p></p>
+
+		<br />
+
+		<div class="flex flex-col gap-4 md:flex-row">
+			<Card>
+				<CardHeader>
+					<h1 class="text-2xl font-bold">Free</h1>
+				</CardHeader>
+
+				<CardContent></CardContent>
+			</Card>
+
+			<Card>
+				<CardHeader>
+					<h1 class="text-2xl font-bold">Free</h1>
+				</CardHeader>
+
+				<CardContent></CardContent>
+			</Card>
+
+			<Card>
+				<CardHeader>
+					<h1 class="text-2xl font-bold">Free</h1>
+				</CardHeader>
+
+				<CardContent></CardContent>
+			</Card>
+		</div>
+	</div>
+</div>
